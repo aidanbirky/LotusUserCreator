f49523ff038795d80d2029a880953164cbcd35e6
aidanbirky <github@birky.33mail.com>
2019-03-09 14:24:55 +0800

    removed unused code

--- TopBrutesTweeter.py
+++ TopBrutesTweeter.py
@@ -4,6 +4,7 @@
 ## 58 23 * * * [ $(date +\%d) -eq $(echo $(cal) | awk '{print $NF}') ] && python /var/log/topbrutes/topbrutes.py
 ## Written by Aidan
 ## Last modified 29 December 2015
+##
 
 import os
 import datetime
@@ -25,22 +26,15 @@ def main():
     date = datetime.date.today()
     strDate = date.strftime('%d%m%y')
     sshfirstDay = date.strftime('%Y' + '-' + '%m' + '-' + '01')
-    #createMessage = '''echo " Top SSH Brutes @twitterusername" > /var/log/topbrutes/sshtopbrutes{0} && echo " Top HTTP Brutes @twitterusername" > /var/log/topbrutes/httptopbrutes{0}'''.format(strDate)
     createMessage = '''echo " Top SSH Brutes @twitterusername" > /var/log/topbrutes/sshtopbrutes{0}'''.format(strDate)
     sshTopBrutes = "journalctl -u sshd --since {0} | grep 'Failed' | grep -o ".format(sshfirstDay) + r"'[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'" + " | sort | uniq -c | sort -urn | head -n 3 >> /var/log/topbrutes/sshtopbrutes{0}".format(strDate)
-    #httpTopBrutes = '''cat /var/log/httpd/access_log | cut -d " " -f 1 | sort | uniq -c | sort -urn | head -n 3 >> /var/log/topbrutes/httptopbrutes'''+strDate
     os.system(createMessage)
     os.system(sshTopBrutes)
-    #os.system(httpTopBrutes)
     api = get_api(cfg)
     sshf = open ("/var/log/topbrutes/sshtopbrutes"+strDate, 'r')
     sshtweet = sshf.read()[1:]
     sshstatus = api.update_status(sshtweet)
     sshf.close()
-    #httpf = open ("/var/log/topbrutes/httptopbrutes"+strDate, 'r')
-    #httptweet = httpf.read()[1:]
-    #httpstatus = api.update_status(httptweet)
-    #httpf.close()
 
 if __name__ == "__main__":
     main()
